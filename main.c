09e85a516adf8b9842e6f5325467c9659f730dcb
Hamza El Omrani <hel-omra@e3r1p3.1337.ma>
2024-03-31 10:39:31 +0000

    latest update

--- main.c
+++ main.c
@@ -1,5 +1,31 @@
-#include <unistd.h>
 #include <libc.h>
+#include <unistd.h>
+
+void	free2d(char **s, int i)
+{
+	while (i > -1)
+		free(s[i--]);
+	free(s);
+}
+
+int	ft_count_words(char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s != '\0')
+			i++;
+		while (*s != c && *s != '\0')
+			s++;
+	}
+	return (i);
+}
 
 char	*ft_substr(char *s, unsigned int start, size_t len)
 {
@@ -20,6 +46,36 @@ char	*ft_substr(char *s, unsigned int start, size_t len)
 	new[i] = '\0';
 	return (new);
 }
+
+char	**ft_split(char *s, char c)
+{
+	char	**new;
+	int		k;
+	int		i;
+	int		j;
+
+	k = 0;
+	i = 0;
+	j = 0;
+	new = malloc ((ft_count_words(s, c) + 1) * sizeof(char *));
+	if (!new)
+		return (NULL);
+	while (k < ft_count_words(s, c))
+	{
+		while (s[i] == c && s[i] != '\0')
+			i++;
+		j = i;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+		new[k] = ft_substr(s, j, i - j);
+		if (!new[k])
+			free2d(new, k);
+		k++;
+	}
+	new[k] = NULL;
+	return (new);
+}
+
 
 int ft_strlen(char *s)
 {
@@ -43,11 +99,72 @@ char *trim_end(char *s)
 	new = ft_substr(s, 0, i + 1);
 	return (new);
 }
+
+char	*ft_strjoin(char *s, char *buffer)
+{
+	char	*string;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	// if (!s)
+	// 	return (ft_strdup(buffer));
+	string = malloc(ft_strlen(s) + ft_strlen(buffer) + 1);
+	while (s[i])
+	{
+		string[i] = s[i];
+		i++;
+	}
+	while (buffer[j])
+	{
+		string[i] = buffer[j];
+		i++;
+		j++;
+	}
+	string[i] = '\0';
+	return (string);
+}
+
+int	is_quote(char *s)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (s[j])
+	{
+		if (s[j] == 39)
+		{
+			if (s[j + 1] == 39)
+				return (2);
+			i++;
+		} 
+		j++;
+	}
+	if (i == 1)
+		return (2);
+	if (i == 2)
+		return (1);
+	return (0);
+}
 int main(int ac, char **av, char **env) 
 {
-	// char **f_command = ft_split(av[2], is_quote(av[2]));
-	// // char *a[] = {"grep       ", "2", NULL};
-	// if (execve("/usr/bin/grep", f_command, env) < 0)
-	// 	perror("error :");
-	printf ("{%s}", trim_end(av[1]));
+	char **command;
+	// printf ("{%s}\n", trim_end(av[2]));
+	if (is_quote(av[2]) == 0)
+		command = ft_split(av[2], ' ');
+	else if (is_quote(av[2]) == 1)
+		command = ft_split(av[2], 39);
+	else if (is_quote(av[2]) == 2)
+		printf("%s error quotes", av[2]), exit(1);
+	// char *a[] = {"grep", "'", NULL};
+	char *path = trim_end(*command);
+	path = ft_strjoin("/usr/bin/",path);
+	if (execve(path, command, env) < 0)
+		perror(path);
+	// while (*command)
+	// 	printf("{%s}\n", *command++);
+		// printf("{%s}\n", path);
 }
--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: hel-omra <hel-omra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 05:16:40 by hel-omra          #+#    #+#             */
-/*   Updated: 2024/03/31 07:06:01 by hel-omra         ###   ########.fr       */
+/*   Updated: 2024/03/31 10:37:52 by hel-omra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,48 +66,86 @@ char	**get_path(char **env)
 
 void	child_1(t_vrs *pipex, char **av, char **env)
 {
-	char	**f_command;
-	char	*command;
+	char	**command;
+	char	*path;
 	int		j;
 
 	j = 0;
-	command = trim_end(av[2]);
+	path = trim_end(av[2]);
 	close(pipex->p[0]);
-	pipex->fd_infile = open(av[1], O_CREAT | O_RDONLY, 444);
+	pipex->fd_infile = open(av[1], O_CREAT | O_RDONLY, 0555);
 	if (pipex->fd_infile < 0)
-		ft_error("infile ", &pipex);
+		ft_error("infile ", pipex);
 	dup2(pipex->fd_infile, 0);
-	f_command = ft_split(command, is_quote(command));
-	dup2(pipex->fd_infile, 0);
-
-	while (pipex->path[j])
+	dup2(pipex->p[1], 1);
+	command = ft_split(path, is_quote(path));
+	if (access(command[j], F_OK | X_OK) == 0)
+		path = ft_strdup(command[j]);
+	else
 	{
-		if (access(command, F_OK | X_OK))
-			command = ft_strjoin(pipex->path[j], *f_command);
-		if (access(command, F_OK | X_OK) == 0)
-			break ;
-		j++;
-		if (!pipex->path[j])
-			perror(pipex->path[j]);
-		execve(command, f_command, env);
-		exit (0);
+		while (pipex->path[j])
+		{
+			path = ft_strjoin(pipex->path[j], command[0]);
+			if (access(path, F_OK | X_OK) == 0)
+				break ;
+			j++;
+			free(path);
+			if (!pipex->path[j])
+				perror(pipex->path[j]), exit(1);
+		}
 	}
+			execve(path, command, env);
+}
+
+
+void	child_2(t_vrs *pipex, char **av, char **env)
+{
+	char	**command;
+	char	*path;
+	int		j;
+
+	j = 0;
+	path = trim_end(av[3]);
+	close(pipex->p[1]);
+	pipex->fd_infile = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0555);
+	if (pipex->fd_outfile < 0)
+		ft_error("Outfile ", pipex);
+	dup2(pipex->p[0], 0);
+	dup2(pipex->fd_outfile, 1);
+	command = ft_split(path, is_quote(path));
+	if (access(command[j], F_OK | X_OK) == 0)
+		path = ft_strdup(command[j]);
+	else
+	{
+		while (pipex->path[j])
+		{
+			path = ft_strjoin(pipex->path[j], command[0]);
+			if (access(path, F_OK | X_OK) == 0)
+				break ;
+			j++;
+			free(path);
+			if (!pipex->path[j])
+				perror(pipex->path[j]), exit(1);
+		}
+	}
+			execve(path, command, env);
 }
 
 int	main(int ac, char **av, char **env)
 {
 	t_vrs	pipex;
-	char	**path;
 	int		pid1;
+	int		pid2;
 
 	if (ac != 5)
 		return (1);
 	pipex.path = get_path(env);
 	pipe(pipex.p);
-	dup2(0, pipex.p[1]);
-	close(pipex.p[0]);
+	// dup2(0, pipex.p[1]);
 	pid1 = fork();
 	if (pid1 == 0)
 		child_1(&pipex, av, env);
-	
+	pid2 = fork();
+	if (pid2 == 0)
+		child_2(&pipex, av, env);	
 }
